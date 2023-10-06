bbf266123fc6d9b56d2ba42c42c74329087f2575
Konstantin Lukyanenok <loukianen@gmail.com>
2023-10-06 17:08:36 +0300

    start

---
+++ src/routes/battleship/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+    <link rel="icon" href="./favicon-ship.png" />
+    <title>Battleship</title>
+    <meta name="description" content="Battleship game" />
+</svelte:head>
+
+
+<h1>There will be battleship game</h1>
